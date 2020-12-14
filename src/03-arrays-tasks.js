8d5e2f7411672120748c63d1ff585223eb434baf
ana-dav <opa.super.op@gmail.com>
2020-12-14 20:10:18 +0200

    commit 2

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.findIndex((search) => search === value);
 }
 
 /**
@@ -35,8 +35,15 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const result = [];
+  const length = len * 2 - 1;
+  for (let i = 1; i <= length; i += 1) {
+    if (i % 2) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 
